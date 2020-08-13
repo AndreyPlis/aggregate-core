6bd684d6626a75fe2a18e3b35034132bcad9cd6e
Andrey Kocherov <andrey.kocherov@tibbo.com>
2020-08-13 21:55:37 +0300

    Realization of equals(), hashCode(), clone()

--- src/main/java/com/tibbo/datatable/FieldFormat.java
+++ src/main/java/com/tibbo/datatable/FieldFormat.java
@@ -42,16 +42,44 @@ public abstract class FieldFormat<T> implements Cloneable{
 
     @Override
     public boolean equals(Object o) {
-        throw new UnsupportedOperationException();
+        //Remove placeholder: throw new UnsupportedOperationException();
+        if(o == this) {
+            return true;
+        }
+        if(o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        FieldFormat<T> fieldFormat = (FieldFormat<T>)o;
+        return (name == fieldFormat.name || (name != null && name.equals(fieldFormat.getName())) )
+                ||
+                (description == fieldFormat.description || (description != null && description.equals(fieldFormat.getDescription())) )
+                ||
+                (nullable == fieldFormat.nullable || (nullable != null && nullable.equals(fieldFormat.getNullable())) );
     }
 
     @Override
     public int hashCode() {
-        throw new UnsupportedOperationException();
+        //I think, it was placeholder too: throw new UnsupportedOperationException();
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (name == null ? 0 : name.hashCode());
+        result = prime * result + (description == null ? 0 : description.hashCode());
+        result = prime * result + (nullable == null ? 0 : nullable.hashCode());
+        return result;
     }
 
     @Override
     public String toString() {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        String result;
+        result = "name: " + (name == null ? "null" : name) + "; ";
+        result += "description: " + (description == null ? "null" : description) + "; ";
+        result += "nullable: " + (nullable == null ? "null" : nullable.toString()) + ".";
+        return result;
+    }
+
+    @Override
+    public FieldFormat<T> clone() throws CloneNotSupportedException{
+        return (FieldFormat<T>) super.clone();
     }
 }
