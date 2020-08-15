2e9ad050062ad56bbd73dade97458441708c29d9
DemichevD <nonsense05@gmail.com>
2020-08-15 16:18:26 +0300

    Fixed FieldFormatFactory: add function "funcToCreateFieldFormat"

--- src/main/java/com/tibbo/datatable/FieldFormatFactory.java
+++ src/main/java/com/tibbo/datatable/FieldFormatFactory.java
@@ -3,13 +3,7 @@ package com.tibbo.datatable;
 public class FieldFormatFactory {
 
     public static FieldFormat<?> createFieldFormat(Character type, String name, String description, Boolean nullable, Boolean hidden, String defaultValue) {
-        FieldFormat<?> fieldFormat;
-        switch (type) {
-            case FieldFormat.STRING_FIELD -> fieldFormat = new StringFieldFormat();
-            case FieldFormat.BOOLEAN_FIELD -> fieldFormat = new BooleanFieldFormat();
-            case FieldFormat.INTEGER_FIELD -> fieldFormat = new IntFieldFormat();
-            default -> throw new IllegalArgumentException("Wrong FieldFormat type" + type);
-        }
+        FieldFormat<?> fieldFormat = funcToCreateFieldFormat(type);
         fieldFormat.setName(name);
         fieldFormat.setDescription(description);
         fieldFormat.setNullable(nullable);
@@ -17,68 +11,48 @@ public class FieldFormatFactory {
         fieldFormat.setDefaultValue(defaultValue);
         return fieldFormat;
     }
-//    public static FieldFormat<?> createFieldFormat(Character type, String name, String description, Boolean nullable, Boolean hidden) {
-//        FieldFormat<?> fieldFormat;
-//        switch (type) {
-//            case FieldFormat.STRING_FIELD -> fieldFormat = new StringFieldFormat();
-//            case FieldFormat.BOOLEAN_FIELD -> fieldFormat = new BooleanFieldFormat();
-//            case FieldFormat.INTEGER_FIELD -> fieldFormat = new IntFieldFormat();
-//            default -> throw new IllegalArgumentException("Wrong FieldFormat type" + type);
-//        }
-//        fieldFormat.setName(name);
-//        fieldFormat.setDescription(description);
-//        fieldFormat.setNullable(nullable);
-//        fieldFormat.setHidden(hidden);
-//        return fieldFormat;
-//    }
-//
-//    public static FieldFormat<?> createFieldFormat(Character type, String name, String description, Boolean nullable) {
-//        FieldFormat<?> fieldFormat;
-//        switch (type) {
-//            case FieldFormat.STRING_FIELD -> fieldFormat = new StringFieldFormat();
-//            case FieldFormat.BOOLEAN_FIELD -> fieldFormat = new BooleanFieldFormat();
-//            case FieldFormat.INTEGER_FIELD -> fieldFormat = new IntFieldFormat();
-//            default -> throw new IllegalArgumentException("Wrong FieldFormat type" + type);
-//        }
-//        fieldFormat.setName(name);
-//        fieldFormat.setDescription(description);
-//        fieldFormat.setNullable(nullable);
-//        return fieldFormat;
-//    }
-//
-//    public static FieldFormat<?> createFieldFormat(Character type, String name, String description) {
-//        FieldFormat<?> fieldFormat;
-//        switch (type) {
-//            case FieldFormat.STRING_FIELD -> fieldFormat = new StringFieldFormat();
-//            case FieldFormat.BOOLEAN_FIELD -> fieldFormat = new BooleanFieldFormat();
-//            case FieldFormat.INTEGER_FIELD -> fieldFormat = new IntFieldFormat();
-//            default -> throw new IllegalArgumentException("Wrong FieldFormat type" + type);
-//        }
-//        fieldFormat.setName(name);
-//        fieldFormat.setDescription(description);
-//        return fieldFormat;
-//    }
-//
-//    public static FieldFormat<?> createFieldFormat(Character type, String name) {
-//        FieldFormat<?> fieldFormat;
-//        switch (type) {
-//            case FieldFormat.STRING_FIELD -> fieldFormat = new StringFieldFormat();
-//            case FieldFormat.BOOLEAN_FIELD -> fieldFormat = new BooleanFieldFormat();
-//            case FieldFormat.INTEGER_FIELD -> fieldFormat = new IntFieldFormat();
-//            default -> throw new IllegalArgumentException("Wrong FieldFormat type" + type);
-//        }
-//        fieldFormat.setName(name);
-//        return fieldFormat;
-//    }
-//
-//    public static FieldFormat<?> createFieldFormat(Character type) {
-//        FieldFormat<?> fieldFormat;
-//        switch (type) {
-//            case FieldFormat.STRING_FIELD -> fieldFormat = new StringFieldFormat();
-//            case FieldFormat.BOOLEAN_FIELD -> fieldFormat = new BooleanFieldFormat();
-//            case FieldFormat.INTEGER_FIELD -> fieldFormat = new IntFieldFormat();
-//            default -> throw new IllegalArgumentException("Wrong FieldFormat type" + type);
-//        }
-//        return fieldFormat;
-//    }
+    public static FieldFormat<?> createFieldFormat(Character type, String name, String description, Boolean nullable, Boolean hidden) {
+        FieldFormat<?> fieldFormat = funcToCreateFieldFormat(type);
+        fieldFormat.setName(name);
+        fieldFormat.setDescription(description);
+        fieldFormat.setNullable(nullable);
+        fieldFormat.setHidden(hidden);
+        return fieldFormat;
+    }
+
+    public static FieldFormat<?> createFieldFormat(Character type, String name, String description, Boolean nullable) {
+        FieldFormat<?> fieldFormat = funcToCreateFieldFormat(type);
+        fieldFormat.setName(name);
+        fieldFormat.setDescription(description);
+        fieldFormat.setNullable(nullable);
+        return fieldFormat;
+    }
+
+    public static FieldFormat<?> createFieldFormat(Character type, String name, String description) {
+        FieldFormat<?> fieldFormat = funcToCreateFieldFormat(type);
+        fieldFormat.setName(name);
+        fieldFormat.setDescription(description);
+        return fieldFormat;
+    }
+
+    public static FieldFormat<?> createFieldFormat(Character type, String name) {
+        FieldFormat<?> fieldFormat = funcToCreateFieldFormat(type);
+        fieldFormat.setName(name);
+        return fieldFormat;
+    }
+
+    public static FieldFormat<?> createFieldFormat(Character type) {
+        return funcToCreateFieldFormat(type);
+    }
+
+    private static FieldFormat<?> funcToCreateFieldFormat(Character type) {
+        FieldFormat<?> fieldFormat;
+        switch (type) {
+            case FieldFormat.STRING_FIELD -> fieldFormat = new StringFieldFormat();
+            case FieldFormat.BOOLEAN_FIELD -> fieldFormat = new BooleanFieldFormat();
+            case FieldFormat.INTEGER_FIELD -> fieldFormat = new IntFieldFormat();
+            default -> throw new IllegalArgumentException("Wrong FieldFormat type" + type);
+        }
+        return fieldFormat;
+    }
 }
