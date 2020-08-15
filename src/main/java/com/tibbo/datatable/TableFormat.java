3761fcddc1fde73cc2b8b8e004e21eb9793b2b37
Andrey Kocherov <andrey.kocherov@tibbo.com>
2020-08-15 23:01:11 +0300

    1. Realization of Tests for equals(), hashCode(), clone() Realization of Abstract Factory pattern: - abstract base factory class FieldFormatCreator; - concrete creators StringFieldFormatCreator and IntFieldFormatCreator. BooleanFieldFormat was added. Fields 'hidden' and 'defaultValue' was added. Validtaros still in progress. 2. equals, hashCode, toString, clone was added to TableFormat. Field for minimal and maximal rows quantity was added.

--- src/main/java/com/tibbo/datatable/TableFormat.java
+++ src/main/java/com/tibbo/datatable/TableFormat.java
@@ -4,15 +4,57 @@ import java.util.*;
 
 public class TableFormat implements Cloneable {
     private List<FieldFormat> fields = new ArrayList<>();
+    private int minRows = 0;
+    private int maxRows = Integer.MAX_VALUE;
 
 
     public void addField(FieldFormat fieldFormat)
     {
-        fields.add(fieldFormat);
+        if(fields.size() < maxRows) {
+            fields.add(fieldFormat);
+        }
     }
 
     public boolean removeField(FieldFormat fieldFormat)
     {
-        return fields.remove(fieldFormat);
+        if( fields.size() > minRows) {
+            return fields.remove(fieldFormat);
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o == this) {
+            return true;
+        }
+        if(o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        TableFormat tableFormat = (TableFormat)o;
+        return fields.equals(tableFormat.fields);
+                //(fields == tableFormat.fields || (fields != null && fields.equals(tableFormat.fields)) );
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (fields == null ? 0 : fields.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        String result;
+        result = "fields: " + (fields == null ? "null" : fields.toString()) + ".";
+        return result;
+    }
+
+    @Override
+    public TableFormat clone() throws CloneNotSupportedException{
+        return (TableFormat) super.clone();
     }
 }
