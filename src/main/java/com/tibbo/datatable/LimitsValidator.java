8fc1d9dc603862e03be3c1c90133665070a8af69
K1ZO <lexacv01@gmail.com>
2020-08-23 21:30:48 +0300

    Create LimitsValidator

---
+++ src/main/java/com/tibbo/datatable/LimitsValidator.java
@@ -0,0 +1,18 @@
+package com.tibbo.datatable;
+
+public class LimitsValidator<T> implements FieldValidator<T> {
+    private final int startValueLimits;
+    private final int endValueLimits;
+
+    public LimitsValidator(int startValueLimits, int endValueLimits) {
+        this.startValueLimits = startValueLimits;
+        this.endValueLimits = endValueLimits;
+    }
+
+    @Override
+    public void validate(T value) throws ValidationException {
+        if((int)value <= startValueLimits || (int) value>= endValueLimits){
+            throw new IllegalArgumentException("The value must be different");
+        }
+    }
+}
